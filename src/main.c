a57f40fadc046f47cfb8eae5d98a9e65efdaaa83
toma <bricout.thomas@mailfence.com>
2023-01-26 20:09:05 +0100

    wrote a * triangle programm with user input and for logic

--- src/main.c
+++ src/main.c
@@ -3,6 +3,18 @@
 
 int main(int argc, char *argv[])
 { 
-  printf("Hello world!\n");
+  // ask the user for the size of the triangle
+  int triangle_size = 0;
+  printf("Insert triangle size:");
+  scanf("%d", &triangle_size);
+  
+  // draw the triangle
+  for (int i=1; i <= triangle_size; i++) {
+    for (int j=0; j < i; j++) {
+      printf("*");
+    }
+    printf("\n");
+  }
+
   return (EXIT_SUCCESS);
 }
